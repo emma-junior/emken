d2b939e36fa24f4533d90de1c43bc0f6f674407e
emma-junior <emnzboy31@gmail.com>
2023-02-10 05:23:38 +0800

    implemented responsiveness for navbar and hero

--- client/src/components/hero/Hero.tsx
+++ client/src/components/hero/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../button/Button'
 import "./hero.scss"
 
 const Hero = () => {
@@ -7,7 +8,7 @@ const Hero = () => {
       <div className='hero-content'>
         <h1 className='hero-topic'>THE BEST RELIABLE MEDICAL EMERGENCY SERVICE IN YOUR HANDS</h1>
         <p>Dedicated to helping you with all medical issues/emergency 24/7</p>        
-        <button>Get Started</button>
+        <Button text="Get Started" />
       </div>
       <div className='hero-pic'>
         <img src='./assets/images/heroimg.png' alt='' />
--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,8 +1,11 @@
 import React,{useState, useEffect} from 'react'
+import Button from '../button/Button';
 import "./navbar.scss"
+import {FaBars, FaTimes} from "react-icons/fa"
 
 const Navbar = () => {
     const [color, setColor] = useState<boolean>(false);
+    const [click, setClick] = useState<boolean>(false);
 
     const changeColor = () => {
         if (window.scrollY >= 1) {
@@ -12,6 +15,8 @@ const Navbar = () => {
         }
     };
 
+    const handleClick = () => setClick(!click)
+
     useEffect(() => {
 		window.addEventListener("scroll", changeColor);
   }, [])
@@ -19,15 +24,20 @@ const Navbar = () => {
     <nav className={color ? 'navbar navbar-bg' : 'navbar'}>
         <div className='navbar-wrapper'>
             <h3>EMKEN</h3>
-            <ul>
-                <li>HOME</li>
-                <li>MEDICAL EMERGENCY</li>
-                <li>FIND HOSPITAL</li>
-                <li>CHAT WITH DOCTOR</li>
-            </ul>
-            <div className='signup-login'>
-                <p className='signup'>Sign Up</p>
-                <div className='login'><button className='login-btn'>Log In</button></div>
+            <div className={click ? 'show-sidebar' : 'nav-links'}>
+                <ul>
+                    <li>HOME</li>
+                    <li>MEDICAL EMERGENCY</li>
+                    <li>FIND HOSPITAL</li>
+                    <li>CHAT WITH DOCTOR</li>
+                </ul>
+                <div className='signup-login'>
+                    <p className='signup'>Sign Up</p>
+                    <Button text="Log In" />
+                </div>
+            </div>
+            <div className='mobile-icon' onClick={handleClick}>
+                {!click ?  <FaBars /> : <FaTimes />}
             </div>
         </div>
     </nav>
