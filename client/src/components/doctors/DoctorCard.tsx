5f3d0378f77d6240ca5bb3296d3a05cbcb9934ad
emma-junior <emnzboy31@gmail.com>
2023-03-17 22:58:34 +0100

    implemented lazy load img in doctors section

--- client/src/components/doctors/DoctorCard.tsx
+++ client/src/components/doctors/DoctorCard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { doctorsType } from '../../model'
 import "./doctorscard.scss"
 import {FaFacebookF, FaTwitter, FaLinkedinIn} from "react-icons/fa"
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 interface Props {
     doctor: doctorsType
@@ -9,7 +11,14 @@ interface Props {
 const DoctorCard = ({doctor}:Props) => {
   return (
     <section className='card'>
-        <img className='doctor-pic' src={`/assets/images/doctors/${doctor.image}.jpg`} alt='doctor' />
+        {/* <img className='doctor-pic' src={`/assets/images/doctors/${doctor.image}.jpg`} alt='doctor' /> */}
+        <LazyLoadImage
+          className="doctor-pic"
+          src={`/assets/images/doctors/${doctor.image}.jpg`}
+          placeholderSrc="/assets/images/doctors/doc-placeholder.jpg"
+          effect="blur"
+          alt="doctor image"
+        />
         <div className='content'>
             <p className='doctor-name'>{doctor.name}</p>
             <div className='icons'>
