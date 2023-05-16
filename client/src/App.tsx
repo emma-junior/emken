1136565660d76a9c25d077dfd608061220dcd381
emma-junior <emnzboy31@gmail.com>
2023-05-16 19:38:55 +0100

    started the implentation of login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Home from './pages/home/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>    
   );
 }
 
