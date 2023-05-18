20d0f957f2f2139ddd9882486a78f221d512f6e8
emma-junior <emnzboy31@gmail.com>
2023-05-18 19:12:43 +0100

    implemented login page

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./login.scss"
 import Button from '../../components/button/Button'
+import {AiOutlineEye, AiOutlineEyeInvisible} from "react-icons/ai"
+import {FcGoogle} from "react-icons/fc"
+import {FaFacebookSquare} from "react-icons/fa"
 
 const Login = () => {
+    const [visible, setVisible] = useState<boolean>(false)
+    const [input, setInput] = useState<string>("password")
+
+    const togglePassword = () => {
+        setVisible(!visible)
+        setInput(visible ? "password" : "text")
+    }
   return (
     <main className='login'>
         <div className='login_wrapper'>
@@ -20,8 +30,27 @@ const Login = () => {
                             <label>Email</label>
                             <input 
                                 type="email" 
-                                // placeholder="username"
                             />
+                        </div>
+                        <div className='formwrapper_wrapper_form_input password'>
+                            <label>Password</label>
+                            <input 
+                                type={input}
+                            />
+                            {visible ? <p onClick={togglePassword} className='shown-password'><AiOutlineEye /></p>: <p onClick={togglePassword} className='shown-password'><AiOutlineEyeInvisible /></p>}
+                        </div>
+                        <p className='formwrapper_wrapper_form_forgot'>Forgot password?</p>
+                        <button className='formwrapper_wrapper_form_btn'>Sign In Now</button>
+                        <div className='formwrapper_wrapper_form_alternative'>
+                            <p className='formwrapper_wrapper_form_alternative_p'>Or Continue With</p>
+                            <div className='formwrapper_wrapper_form_alternative_signin'>
+                                <div className='formwrapper_wrapper_form_alternative_signin_google'>
+                                    <FcGoogle className='icon'/> <p>Google</p>
+                                </div>
+                                <div className='formwrapper_wrapper_form_alternative_signin_facebook'>
+                                    <FaFacebookSquare className='icon'/> <p>Facebook</p>
+                                </div>
+                            </div>
                         </div>
                     </form>
                 </section>
