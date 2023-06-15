6a3341a2680e1dab4665e9f640b4fbb85d5f0642
emma-junior <emnzboy31@gmail.com>
2023-06-15 14:09:25 +0100

    (refactor): login page

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,6 +1,4 @@
 import React,{useState, FormEvent, useContext} from 'react'
-import "./login.scss"
-import Button from '../../components/button/Button'
 import {AiOutlineEye, AiOutlineEyeInvisible} from "react-icons/ai"
 import {FcGoogle} from "react-icons/fc"
 import {FaFacebookSquare} from "react-icons/fa"
@@ -8,16 +6,19 @@ import {app, database} from '../../firebaseConfig'
 import {getAuth, signInWithEmailAndPassword} from "firebase/auth"
 import { useNavigate } from 'react-router'
 import { AuthContext } from '../../context/AuthContext'
+import Logo from '../../components/logo/Logo'
+import Input from '../../components/input/Input'
+import "../../styles/auth.scss"
 
 const Login = () => {
     const auth = getAuth(app)
-    const [data, setData] = useState<any>({
+    const [data, setData] = useState({
         email: '',
         password: ''
     })
     const {dispatch} = useContext(AuthContext)
     const navigate = useNavigate()
-    const handleInput = (event:any) => {
+    const handleInput = (event:React.ChangeEvent<HTMLInputElement>) => {
         let newInput = { [event.target.name]: event.target.value};
 
         setData({...data, ...newInput})
@@ -35,53 +36,25 @@ const Login = () => {
         })
     }   
 
-    const [visible, setVisible] = useState<boolean>(false)
-    const [input, setInput] = useState<string>("password")
-
-    const togglePassword = () => {
-        setVisible(!visible)
-        setInput(visible ? "password" : "text")
-    }
   return (
-    <main className='login'>
-        <div className='login_top'>
-            <p className='login_top_p'>New to EMKEN?</p>
-            <Button size = 'large' label="Sign Up" />
+    <section className='auth login'>
+        <figure className='auth_image'>
+            <img src='/assets/images/login-image.jpg' alt='register' />
+            <h1 className='quote'>Login to get full access to all our access.</h1>
+            <div className='overlay'></div>
+        </figure>
+        <div className='auth_form'>
+            <div><Logo /></div>
+            <div className='auth_form_input'><Input name="email" handleInput={handleInput}  /></div>
+            <div className='auth_form_input'><Input name='password' handleInput={handleInput}/></div>
+            <br />
+            <button className='auth_form_loginbtn' onClick={handleSubmit}>SIGN UP</button>
+            <button className='auth_form_login-google'>SIGN UP WITH GOOGLE</button>
+            <br />
+            <p className='auth_form_forgotten'>Forgotten your password?</p>
+            <p className='auth_form_account'>Don't have an account? <span className='signup'>Sign up here!</span></p>
         </div>
-        <div className='login_wrapper'>
-            <figure className='login_wrapper_image'><img src='/assets/images/doc-illustration.png' alt='treatment' /></figure>
-            <div className='formwrapper'>
-                <section className='formwrapper_wrapper'>
-                    <form className='formwrapper_wrapper_form'>
-                        <h2>Welcome Back to EMKEN!</h2>
-                        <p className='formwrapper_wrapper_form_p'>Welcome to your medical platform. Please sign in back to get full access to services</p>
-                        <div className='formwrapper_wrapper_form_input'>
-                            <label>Email</label>
-                            <input name='email' placeholder='email' type='email' onChange={(event) => handleInput(event)} />
-                        </div>
-                        <div className='formwrapper_wrapper_form_input password'>
-                            <label>Password</label>
-                            <input name='password' placeholder='password' type={input} onChange={(event) => handleInput(event)} />
-                            {visible ? <p onClick={togglePassword} className='shown-password'><AiOutlineEye /></p>: <p onClick={togglePassword} className='shown-password'><AiOutlineEyeInvisible /></p>}
-                        </div>
-                        <p className='formwrapper_wrapper_form_forgot'>Forgot password?</p>
-                        <button onClick={handleSubmit} className='formwrapper_wrapper_form_btn'>Sign In Now</button>
-                        <div className='formwrapper_wrapper_form_alternative'>
-                            <p className='formwrapper_wrapper_form_alternative_p'>Or Continue With</p>
-                            <div className='formwrapper_wrapper_form_alternative_signin'>
-                                <div className='formwrapper_wrapper_form_alternative_signin_google'>
-                                    <FcGoogle className='icon'/> <p>Google</p>
-                                </div>
-                                <div className='formwrapper_wrapper_form_alternative_signin_facebook'>
-                                    <FaFacebookSquare className='icon'/> <p>Facebook</p>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </section>
-            </div>
-        </div>
-    </main>
+    </section>
   )
 }
 
