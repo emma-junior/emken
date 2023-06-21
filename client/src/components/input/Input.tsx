1cb2a2d7d99231834a7cd4744c5b2e72ba0833a1
emma-junior <emnzboy31@gmail.com>
2023-06-21 22:05:44 +0100

    (chore): started refactoring context hook

--- client/src/components/input/Input.tsx
+++ client/src/components/input/Input.tsx
@@ -3,13 +3,14 @@ import "./input.scss"
 
 interface Props {
   name: string;
+  type: string;
   handleInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({name, handleInput}:Props) => {
+const Input = ({name, type, handleInput}:Props) => { 
   return (
     <div className='input-wrapper'>
-        <input name={name} type={name} onChange={(event) => handleInput(event)} />
+        <input name={name} type={type} onChange={(event) => handleInput(event)} />
         <label htmlFor="name" className='label-name'>
             <span className='content-name'>{name}</span>        
         </label>
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,13 +1,19 @@
 import React, {useState} from 'react'
 import {app} from '../../firebaseConfig'
 import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth"
+import { getFirestore, doc, serverTimestamp, setDoc, } from "firebase/firestore";
 import "../../styles/auth.scss"
 import Input from '../../components/input/Input'
+import { useNavigate } from 'react-router'
 import Logo from '../../components/logo/Logo'
 
 const Register = () => {
     const auth = getAuth(app)
+    const navigate = useNavigate()
+    const db = getFirestore(app);
+    const [loading, setLoading] = useState(false)
     const [data, setData] = useState({
+        displayName: '',
         email: '',
         password: ''
     })
@@ -17,23 +23,42 @@ const Register = () => {
 
         setData({...data, ...newInput})
     }
-    const handleSubmit = () => {
-        createUserWithEmailAndPassword(auth, data.email, data.password)
-        .then((response) => {
-            console.log(response.user)
-        })
-        .catch((err) => {
+    const handleSubmit = async(e:any) => {
+        e.preventDefault();
+        setLoading(true)
+        try {
+            const res = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            );
+            await setDoc(doc(db, "users", res.user.uid), {
+                ...data,
+                uid: res.user.uid,
+                timeStamp: serverTimestamp(),
+            });
+            // await setDoc(doc(db, "userChats", res.user.uid), {});
+            setLoading(false)
+            navigate("/login")
+        }
+        // createUserWithEmailAndPassword(auth, data.email, data.password)
+        // .then((response) => {
+        //     console.log(response.user)
+        // })
+        catch (err:any) {
+            setLoading(false)
             alert(err.message)
-        })
+        }
     }   
   return (
     <section className='auth register'>
         <div className='auth_form'>
             <div><Logo /></div>
-            <div className='auth_form_input'><Input name="email" handleInput={handleInput}  /></div>
-            <div className='auth_form_input'><Input name='password' handleInput={handleInput}/></div>
+            <div className='auth_form_input'><Input name="displayName" type='text' handleInput={handleInput}  /></div>
+            <div className='auth_form_input'><Input name="email" type="email" handleInput={handleInput}  /></div>
+            <div className='auth_form_input'><Input name='password' type='password' handleInput={handleInput}/></div>
             <br />
-            <button className='auth_form_loginbtn' onClick={handleSubmit}>SIGN UP</button>
+            <button className='auth_form_loginbtn' onClick={handleSubmit}>{loading ? "LOADING...":"SIGN UP"}</button>
             <button className='auth_form_login-google'>SIGN UP WITH GOOGLE</button>
         </div>
         <figure className='auth_image'>
