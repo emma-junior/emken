a219cd68c73f25b4faf05655868b729d28f5945c
emma-junior <emnzboy31@gmail.com>
2023-07-01 17:30:46 +0100

    (style): chat page

--- client/src/components/logo/Logo.tsx
+++ client/src/components/logo/Logo.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import "./logo.scss"
-import {FaBriefcaseMedical} from "react-icons/fa"
 
 const Logo = () => {
   return (
     <div className='logo'>
-        {/* <FaBriefcaseMedical className='logo-icon' /> */}
         <h1>EMKEN</h1>
     </div>
   )
