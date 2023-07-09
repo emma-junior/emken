95c8d28de75de21e02c415c6607054eab8f7d655
emma-junior <emnzboy31@gmail.com>
2023-07-09 19:51:49 +0100

    (chore): implemented available doctors in chat

--- client/src/data/testimonials.ts
+++ client/src/data/testimonials.ts
@@ -9,35 +9,35 @@ export interface testimonialsType {
 export const testimonials:testimonialsType[] = [
     {
         id: 1,
-        image: "doctor-three",
+        image: "dr-johnson",
         name: "Okafor Peters",
         rating: "",
         comment: "This medical emergency web app is an absolute lifesaver! It's incredibly user-friendly and provides step-by-step guidance on what to do in different emergency situations."
     },
     {
         id: 2,
-        image: "doctor-three",
+        image: "dr-johnson",
         name: "Adewale John",
         rating: "",
         comment: "I applaud the creators of this medical emergency app for their innovation and dedication to saving lives. The app accurately gave me location to a hospital during an emergency,"
     },
     {
         id: 3,
-        image: "doctor-three",
+        image: "dr-johnson",
         name: "Mrs Hapiness Ndubisi",
         rating: "",
         comment: "As a parent, I feel a tremendous sense of relief knowing that this medical emergency app is just a tap away. It's comforting to know that if anything happens to my child, I can quickly access emergency services and provide them with crucial information."
     },
     {
         id: 4,
-        image: "doctor-three",
+        image: "dr-johnson",
         name: "Mrs Hapiness Ndubisi",
         rating: "",
         comment: "As a parent, I feel a tremendous sense of relief knowing that this medical emergency app is just a tap away. It's comforting to know that if anything happens to my child, I can quickly access emergency services and provide them with crucial information."
     },
     {
         id: 5,
-        image: "doctor-three",
+        image: "dr-johnson",
         name: "Mrs Hapiness Ndubisi",
         rating: "",
         comment: "As a parent, I feel a tremendous sense of relief knowing that this medical emergency app is just a tap away. It's comforting to know that if anything happens to my child, I can quickly access emergency services and provide them with crucial information."
