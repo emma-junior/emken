2d09a8d28af9d9248cb5277a625ec519a08d2415
emma-junior <emnzboy31@gmail.com>
2023-12-04 11:41:55 +0100

    implemented responsiveness for small desktops

--- client/src/components/hero/Hero.tsx
+++ client/src/components/hero/Hero.tsx
@@ -3,14 +3,16 @@ import "./hero.scss"
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { FloaterOne, FloaterTwo, FloaterThree } from './Floaters';
+import { useNavigate } from 'react-router-dom'
 
 const Hero = () => {
+  const navigate = useNavigate()
   return (
     <section className='hero'>
       <div className='hero-content'>
         <h1 className='hero-topic'>THE BEST RELIABLE MEDICAL EMERGENCY SERVICE IN YOUR HANDS</h1>
         <p>Dedicated to helping you with all medical issues/emergency 24/7</p>
-        <Button size = 'medium' label="Get Started" />
+        <Button size = 'medium' label="Get Started" onClick={() => navigate('/register')} />
       </div>
       <div className='hero-pic'>
         <div className='floating-content-one'><FloaterOne /></div>
