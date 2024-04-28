fe6f4d435b73fc5254e46a09d712dc93a6bb1e9f
emma-junior <emnzboy31@gmail.com>
2024-04-28 08:31:46 +0100

    changed overlays color

--- client/src/components/doctors/Doctors.tsx
+++ client/src/components/doctors/Doctors.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import { doctors } from '../../data/doctors'
-import DoctorCard from '../doctorcard/DoctorCard'
-import "./doctors.scss"
+import React from "react";
+import { doctors } from "../../data/doctors";
+import DoctorCard from "../doctorcard/DoctorCard";
+import "./doctors.scss";
+import { motion } from "framer-motion";
+import useAnimate from "../../animations";
 
 const Doctors = () => {
+  const { ref, animationUp } = useAnimate();
   return (
-    <section className='doctors-container'>
-        <p className='title'>OUR DOCTORS</p>
-        <p className='meet'>Meet Our Professional Doctors</p>
-        <div className='doctors'>{doctors.map((doctor) => <DoctorCard key={doctor.id} doctor={doctor} />)}</div>
-    </section>
-  )
-}
+    <motion.section
+      ref={ref}
+      animate={animationUp}
+      className="doctors-container"
+    >
+      <p className="title">OUR DOCTORS</p>
+      <p className="meet">Meet Our Professional Doctors</p>
+      <div className="doctors">
+        {doctors.map((doctor) => (
+          <DoctorCard key={doctor.id} doctor={doctor} />
+        ))}
+      </div>
+    </motion.section>
+  );
+};
 
-export default Doctors
+export default Doctors;
